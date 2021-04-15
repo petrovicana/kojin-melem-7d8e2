4982c552e59e4a9051e0ab1e9edeb0bf81b35f4b
Ana Petrovic <anapetrovic9994@gmail.com>
2021-04-15 09:48:36 +0200

    github pages configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const siteMetadata = require('./site-metadata.json')
 
 module.exports = {
-    pathPrefix: '/',
+    pathPrefix: '/kojin-melem-7d8e2',
     siteMetadata: siteMetadata,
     plugins: [
         `gatsby-plugin-react-helmet`,
